29c65c426ff2d502809e01c81953f8537bb883d1
shin1rok <skssikm@gmail.com>
2018-07-19 01:46:27 +0900

    新規投稿フォーム作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to "/posts/index"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'post/index' => "post#index"
-  get 'post/new' => "post#new"
+  get 'posts/index' => "posts#index"
+  get 'posts/new' => "posts#new"
+  post 'posts/create' => "posts#create"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
