7779e6544ecb1db20df3735772adb40c2174ce6b
shin1rok <skssikm@gmail.com>
2018-07-19 00:37:22 +0900

    postモデル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+99.times do |n|
+  content = n
+  Post.create!(content: content)
+end
