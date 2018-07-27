92502b3d0f5312302d63f98b1bb2bacc42e71557
shin1rok <skssikm@gmail.com>
2018-07-27 14:45:04 +0900

    コメント修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   validates :provider, presence: true
   validates :uid, presence: true
 
-  # 引数に関連するユーザーが存在すればそれを返し、存在しまければ新規に作成する
+  # 引数に関連するユーザーが存在すればそれを返し、存在しなければ新規に作成する
   def self.find_or_create_from_auth_hash(auth_hash)
-  # OmniAuthで取得した各データを代入していく
+  # OmniAuthで取得した各データを代入する
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
     nickname = auth_hash[:info][:nickname]
